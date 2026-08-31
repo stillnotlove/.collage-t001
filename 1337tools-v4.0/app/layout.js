6fbe89e71c8e575a1aba0095a191e8bb926d3e5f
stillnotlove <dankob1337@gmail.com>
2026-09-01 00:01:46 +0300

    Delete 1337tools-v4.0 directory

--- 1337tools-v4.0/app/layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'1337tools 4.0',description:'Experimental graphic systems: FIELD + COMPOSE'};
-export default function RootLayout({children}){return <html lang="ru"><body>{children}</body></html>}
