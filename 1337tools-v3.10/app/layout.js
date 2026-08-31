e5f0bcba9db03f854fc9b99c1339d690b196c939
stillnotlove <dankob1337@gmail.com>
2026-08-31 18:13:19 +0300

    Delete 1337tools-v3.10 directory

--- 1337tools-v3.10/app/layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'1337tools 3.10',description:'Experimental graphic playground'};
-export default function RootLayout({children}){return <html lang="ru"><body>{children}</body></html>}
