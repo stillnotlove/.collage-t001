fdcc0a9a8538dad1514b602d67bccd5fe5cc8ed7
stillnotlove <dankob1337@gmail.com>
2026-09-02 16:52:28 +0300

    Delete 1337tools-v4.17 directory

--- 1337tools-v4.17/app/layout.js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'1337tools 4.17',description:'1337tools'};
-export default function RootLayout({children}){return <html lang="ru"><body>{children}</body></html>}
